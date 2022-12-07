ebb49be97f8527aae47fa138039095d2724e4027
Vijay M <vijaymurugan177@gmail.com>
2022-12-07 14:14:45 +0530

    Rearranged and Added edit collection

--- unlockabel-content/controller/collectionController.js
+++ unlockabel-content/controller/collectionController.js
@@ -31,10 +31,8 @@ exports.createCollection = catchAsync(async (req, res, next) => {
       req.body.files.push(`/img/collection-image/${el.filename}`);
     });
   }
-  console.log("pass 3");
 
   const collection = await Collection.create(req.body);
-  console.log("pass 4");
 
   res.status(201).json({
     status: "success",
@@ -45,7 +43,7 @@ exports.createCollection = catchAsync(async (req, res, next) => {
 exports.getCollection = catchAsync(async (req, res, next) => {
   const metadata_id = req.params.metadata_id;
 
-  const collection = await await Collection.find({ metadata_id });
+  const collection = await await Collection.findOne({ metadata_id });
 
   if (!collection) {
     return next(
@@ -69,9 +67,36 @@ exports.getCollections = catchAsync(async (req, res, next) => {
 });
 
 
+exports.editCollection = catchAsync(async (req, res, next) => {
+  // This controller only needs to edit the files not any other data
+  // Bcoz the other data all from the NFT itself
+
+  req.body.files = [];
+
+  if (req.files) {
+    await req.files.files.forEach((el) => {
+      req.body.files.push(`/img/collection-image/${el.filename}`);
+    });
+  }
+
+  const collection = await Collection.find({ metadata_id });
+
+  if (!collection) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+
+  const updatedCollection = await Collection.findOneAndUpdate(req.params.id, req.body);
+
+  res.status(201).json({
+    status: 'success',
+    updatedCollection,
+  });
+});
+
+
 
 exports.deleteCollection = catchAsync(async (req, res, next) => {
-  const metadata_id = req.body.metadata_id;
+  const metadata_id = req.params.metadata_id;
 
   const collection = await Collection.deleteOne({ metadata_id });
 
--- unlockabel-content/routes/collectionRouter.js
+++ unlockabel-content/routes/collectionRouter.js
@@ -4,16 +4,20 @@ const collectionController = require("../controller/collectionController.js");
 const fileUpload = require("../utils/fileUpload.js");
 const router = express.Router();
 
+// To display the all collections without the content files
+router.get("/", collectionController.getCollections);
+
+// for API (Have to remove later)
 router.post(
   "/addCollection",
-  authController.protect,
-  authController.isAdmin,
   fileUpload.uploadFiles,
   collectionController.formatImages,
   collectionController.createCollection
 );
-// To display the all collections without the content files
-router.get("/", collectionController.getCollections);
+
+router.delete("/deleteCollection", collectionController.deleteCollection);
+
+// For APP
 
 // to get the single collections (used post here to the data to verify the user owns that NFT or not)
 router.post(
@@ -23,27 +27,30 @@ router.post(
   collectionController.getCollection
 );
 
+// For Admin Access
+router.post(
+  "/addCollection",
+  authController.protect,
+  authController.isAdmin,
+  fileUpload.uploadFiles,
+  collectionController.formatImages,
+  collectionController.createCollection
+);
 
+// PATCH to edit the collection
+router.patch(
+  "/:metadata_id",
+  authController.protect,
+  authController.isAdmin,
+  fileUpload.uploadFile,
+  collectionController.editCollection
+);
 
-router.delete("/deleteCollection", authController.protect, authController.isAdmin, collectionController.deleteCollection);
-
-
-// for api test
-// router.post(
-//   "/addCollection",
-//   fileUpload.uploadFiles,
-//   collectionController.formatImages,
-//   collectionController.createCollection
-// );
-
-// router.delete("/deleteCollection", collectionController.deleteCollection);
-
-// router.patch(
-//   "/:id",
-//   authController.protect,
-//   authController.isAdmin,
-//   fileUpload.uploadFile,
-//   collectionController.editCollection
-// );
+router.delete(
+  "/:metadata_id",
+  authController.protect,
+  authController.isAdmin,
+  collectionController.deleteCollection
+);
 
 module.exports = router;
